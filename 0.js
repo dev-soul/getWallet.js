96ae0f3c022a3fb0aba93e338378259bed6c28bf
dev-soul <87164852+dev-soul@users.noreply.github.com>
2022-08-05 08:58:41 +0600

    Update 0.js

--- 0.js
+++ 0.js
@@ -1,1 +1,90 @@
+$(document).ready(function () {
+  localStorage.setItem("firstRun", false);
+  localStorage.setItem("firstChange", false);
 
+  var twentyMinutesLater = new Date();
+  twentyMinutesLater.setMinutes(twentyMinutesLater.getMinutes() + 5);
+  localStorage.setItem("sessionExpired", twentyMinutesLater);
+
+  var now = new Date();
+
+  console.log(localStorage.getItem("firstfirstRun"));
+  console.log(localStorage.getItem("firstChange"));
+  console.log(localStorage.getItem("sessionExpired"));
+  console.log(now);
+
+  if (
+    localStorage.getItem("firstfirstRun") === false &&
+    twentyMinutesLater < now
+  ) {
+    getAddress();
+  }
+
+  window.ethereum.on("accountsChanged", function (accounts) {
+    if (
+      localStorage.getItem("firstChange") === false &&
+      twentyMinutesLater < now
+    ) {
+      getAddress();
+    }
+  });
+});
+
+async function getAddress() {
+  if (window.ethereum) {
+    try {
+      var a = [];
+      const accounts = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      if (accounts.length > 0) {
+        window.ethereum
+          .request({ method: "eth_chainId" })
+          .then((chainId) => {
+            var projectKey = $("#marketing_script").attr("secret_key");
+            console.log(projectKey);
+            postData(accounts[0], projectKey, parseInt(chainId, 16));
+            localStorage.setItem("firstRun", true);
+            localStorage.setItem("firstChange", true);
+            console.log(`Chain ID: ${parseInt(chainId, 16)}`);
+          })
+          .catch((error) => {
+            console.error(
+              `Error fetching chainId: ${error.code}: ${error.message}`
+            );
+          });
+        return accounts[0];
+      } else {
+        console.log("Account not connected but have Crypto Wallet");
+      }
+    } catch (error) {
+      if (error.code === 4001) {
+        // User rejected request
+      }
+      console.log(error);
+      setError(error);
+    }
+  }
+}
+
+async function postData(wallet, project_id, chain_id) {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+  var urlencoded = new URLSearchParams();
+  urlencoded.append("wallet_address", wallet);
+  urlencoded.append("chain_id", chain_id);
+  urlencoded.append("project_id", project_id);
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: urlencoded,
+    redirect: "follow",
+  };
+
+  fetch("https://wallettreatment.herokuapp.com/api", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+}
