050fd83495a4eea4fd39671eadb9183b1f5d50ee
dev-soul <87164852+dev-soul@users.noreply.github.com>
2022-08-05 09:50:49 +0600

    Create finally.js

---
+++ finally.js
@@ -0,0 +1,98 @@
+$(document).ready(function () {
+
+  if (localStorage.getItem("firstRun") === null) {
+    localStorage.setItem("firstRun", false);
+
+    console.log(localStorage.getItem("firstRun"));
+
+  }
+  if (localStorage.getItem("sessionExpired") === null) {
+    var expiredDate = Math.floor(Date.now() / 1000)
+    expiredDate = expiredDate + 1800000
+    localStorage.setItem("sessionExpired", expiredDate);
+    }
+  
+    if (parseInt(localStorage.getItem("sessionExpired")) < now){
+
+      localStorage.setItem("firstRun", false);
+      var expiredDate = Math.floor(Date.now() / 1000)
+      expiredDate = expiredDate + 1800000
+      localStorage.setItem("sessionExpired", expiredDate);
+
+    }
+  var now = Math.floor(Date.now() / 1000)
+
+  console.log(parseInt(localStorage.getItem("sessionExpired")));
+  console.log(now);
+ 
+  
+
+  if (
+    localStorage.getItem("firstRun") === 'false' && parseInt(localStorage.getItem("sessionExpired")) > now
+  ) {
+    getAddress();
+  }
+
+  window.ethereum.on("accountsChanged", function (accounts) {
+    
+      getAddress();
+  });
+});
+
+async function getAddress() {
+  if (window.ethereum) {
+    try {
+      var a = [];
+      const accounts = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      if (accounts.length > 0) {
+        window.ethereum
+          .request({ method: "eth_chainId" })
+          .then((chainId) => {
+            var projectKey = $("#marketing_script").attr("secret_key");
+            console.log(projectKey);
+            postData(accounts[0], projectKey, parseInt(chainId, 16));
+            console.log(`Chain ID: ${parseInt(chainId, 16)}`);
+          })
+          .catch((error) => {
+            console.error(
+              `Error fetching chainId: ${error.code}: ${error.message}`
+            );
+          });
+        return accounts[0];
+      } else {
+        console.log("Account not connected but have Crypto Wallet");
+      }
+    } catch (error) {
+      if (error.code === 4001) {
+        // User rejected request
+      }
+      console.log(error);
+      setError(error);
+    }
+  }
+}
+
+async function postData(wallet, project_id, chain_id) {
+  localStorage.setItem("firstRun", true)
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+  var urlencoded = new URLSearchParams();
+  urlencoded.append("wallet_address", wallet);
+  urlencoded.append("chain_id", chain_id);
+  urlencoded.append("project_id", project_id);
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: urlencoded,
+    redirect: "follow",
+  };
+
+  fetch("https://wallettreatment.herokuapp.com/api", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+}
